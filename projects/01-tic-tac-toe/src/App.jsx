2f239ff47aa5aa90b141e7b970bb0dae72d7b231
vegadev <vegat.kevinj@gmail.com>
2023-04-19 20:08:14 -0400

    Save board an turn in local storage

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
-import confetti from "canvas-confetti";
-import "./App.css";
 import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { Board } from "./components/Board";
+import "./App.css";
+import confetti from "canvas-confetti";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
   // null no hay ganador, false empate
   const [winner, setWinner] = useState(null);
 
@@ -15,6 +23,8 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   };
 
   const updateBoard = (index) => {
@@ -27,6 +37,9 @@ function App() {
     // cambiar de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    //guardar partida
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
     // revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
